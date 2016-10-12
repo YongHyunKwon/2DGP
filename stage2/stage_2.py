fe46020b829b02ea890f74cd7c4634c2b296afcb
k_watch <kwonyong6@naver.com>
2016-10-13 01:25:23 +0900

    stage2
    스테이지 2 시간 적용 및 생명력 적용

--- stage2/stage_2.py
+++ stage2/stage_2.py
@@ -1,5 +1,10 @@
-from pico2d import *
 import random
+import time
+import datetime
+import os
+
+from pico2d import *
+
 
 #***************************************
 # 스테이지 2
@@ -9,6 +14,7 @@ import random
 running     = None
 character   = None
 obstacle    = None
+clear_time  = None
 
 #***************************************
 # Background
@@ -67,10 +73,16 @@ class Character:
         self.frame      = 0
         self.image      = load_image('stage_2_cha.png')
 
+        # ***************************************
+        # 최초 생명력은 3
+        # ***************************************
+        self.life_cnt = 3
+        self.life_image = load_image('heart.png')
+
     def handle_event(self, event):
         # ***************************************
         # ->,<-,↑ 키 입력시 캐릭터 이동
-        # 스테이지 2 에서는 x, y 값을 5 씩 이동
+        # 스테이지 2 에서는 x 값을 5 씩 이동
         # ***************************************
         if event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
@@ -99,8 +111,18 @@ class Character:
         #***************************************
         self.y -= 1
 
+        if (self.life_cnt <= 0):
+            return False
+
     def draw(self):
         self.image.clip_draw(self.frame * 70, 0, 70, 70, self.x, self.y)
+
+        # ***************************************
+        # life_cnt 에 맞게 생명력 이미지를 draw
+        # ***************************************
+        for i in range(0, self.life_cnt):
+            image_range = 30 * (i + 1)
+            self.life_image.draw(image_range, 375)
 
 def handle_events():
     global running
@@ -119,12 +141,56 @@ def handle_events():
         else:
             character.handle_event(event)
 
+#***************************************
+# lifetime
+# 클리어 타임을 출력해주고 0 초에 도달하면
+# 스테이지 완료를 해주는 함수
+#***************************************
+def lifetime():
+    global running
+    global clear_time
+    # ***************************************
+    # 클리어 시간과 현재 시간과의 차이를 구함
+    # ***************************************
+    #########################################
+    # 현재는 테스트용으로 시간을 5 초만 줌
+    #########################################
+    life_time   = clear_time - time.time()
+    str_time    = datetime.datetime.fromtimestamp(life_time).strftime('%S')
+
+    font        = load_font('HMKMRHD.TTF', 20)
+    font.draw(240, 380, str_time)
+
+    # ***************************************
+    # 스테이지 1 의 클리어시간을 버티면 게임 종료
+    # ***************************************
+    if(int(str_time) <= 0 ):
+        clear_font = load_font('HMKMRHD.TTF', 50)
+        clear_font.draw(70, 200, 'Stage Clear')
+        update_canvas()
+        delay(3)
+        running = False
+
+#***************************************
+# stagefail
+# 생명력이 0 이 돼 스테이지를 실패한 상태
+#***************************************
+def stagefail():
+    global running
+
+    clear_font = load_font('HMKMRHD.TTF', 50)
+    clear_font.draw(90, 200, 'Stage Fail')
+    update_canvas()
+    delay(3)
+    running = False
+
 def main():
     open_canvas(500, 400)
 
     global running
     global character
     global obstacle
+    global clear_time
 
     back_ground = Background()
     character   = Character()
@@ -132,7 +198,7 @@ def main():
     # 스테이지 2 에서 장애물은 15개
     #***************************************
     obstacle    = [Obstacle() for i in range(15)]
-
+    clear_time  = time.time() + 5
     running     = True
 
     while running:
@@ -147,6 +213,11 @@ def main():
         for bubble in obstacle:
             bubble.draw()
             bubble.update()
+
+        lifetime()
+
+        if (character.update() == False):
+            stagefail()
 
         update_canvas()
 
