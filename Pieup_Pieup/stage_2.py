8183b8677544c39dfcb3e08d37d4733e8ee67ed6
k_watch <kwonyong6@naver.com>
2016-11-11 00:44:35 +0900

    stage_2_god_item_complete
    스테이지 2 무적 아이템 적용 완료

--- Pieup_Pieup/stage_2.py
+++ Pieup_Pieup/stage_2.py
@@ -35,8 +35,8 @@ class Obstacle:
     # ***************************************
     # 장애물 타입 값
     # ***************************************
-    NONE, HEART, SPEED_UP, TIME_SUB, MOVE_STOP  = 0, 1, 2, 3, 4
-    TIME_ADD, ULTI                              = 5, 6
+    NONE, HEART, SPEED_UP, TIME_SUB, MOVE_STOP      = 0, 1, 2, 3, 4
+    TIME_ADD, ULTI,GOD                              = 5, 6, 7
 
     image           = None
     heart_image     = None
@@ -45,8 +45,10 @@ class Obstacle:
     move_stop_image = None
     time_add_image  = None
     ulti_image      = None
+    god_image       = None
 
     def __init__(self):
+        self.obj = self.NONE
         self.make()
 
         if Obstacle.image == None:
@@ -63,6 +65,8 @@ class Obstacle:
             Obstacle.time_add_image     = load_image('time_add.png')
         if Obstacle.ulti_image == None:
             Obstacle.ulti_image         = load_image('ultimate.png')
+        if Obstacle.god_image == None:
+            Obstacle.god_image          = load_image('god.png')
 
     #***************************************
     # make
@@ -95,6 +99,8 @@ class Obstacle:
             self.obj = self.TIME_ADD
         elif (rand_val < 30):
             self.obj = self.ULTI
+        elif (rand_val < 35):
+            self.obj = self.GOD
         else:
             self.obj = self.NONE
 
@@ -124,6 +130,8 @@ class Obstacle:
             return self.x - 18, self.y - 17, self.x + 16, self.y + 16
         elif (self.obj == self.ULTI):
             return self.x - 28, self.y - 16, self.x + 24, self.y + 25
+        elif (self.obj == self.GOD):
+            return self.x - 28, self.y - 22, self.x + 28, self.y + 20
         else:
             return self.x - 20, self.y - 15, self.x + 15, self.y + 20
 
@@ -140,6 +148,8 @@ class Obstacle:
             self.time_add_image.draw(self.x, self.y)
         elif (self.obj == self.ULTI):
             self.ulti_image.draw(self.x, self.y)
+        elif (self.obj == self.GOD):
+            self.god_image.draw(self.x, self.y)
         else:
             self.image.draw(self.x, self.y)
 
@@ -163,7 +173,7 @@ class Character:
     # 장애물 타입 값
     # ***************************************
     NONE, HEART, SPEED_UP, TIME_SUB, MOVE_STOP  = 0, 1, 2, 3, 4
-    TIME_ADD, ULTI                              = 5, 6
+    TIME_ADD, ULTI, GOD                         = 5, 6, 7
 
     def __init__(self):
         # ***************************************
@@ -381,10 +391,12 @@ class Character:
             self.time_add = True
 
         # ***************************************
-        # 화면 지우기 아이템은 최대 9개 까지만 적용
+        # 스킬은 최대 9개 까지만 적용
         # ***************************************
-        elif (obj == self.ULTI):
+        elif(obj == self.ULTI):
             self.ulti_cnt = min(9, self.ulti_cnt + 1)
+        elif(obj == self.GOD):
+            self.god_cnt = min(9, self.god_cnt + 1)
 
         else:
             # ***************************************
